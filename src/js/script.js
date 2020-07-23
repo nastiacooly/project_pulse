cdf863d6f1502e0a63fa432aa79544088a544ccb
Anastasia <a.maryina@yandex.ru>
2020-07-23 16:09:49 +0300

    added script for mailing of forms

--- src/js/script.js
+++ src/js/script.js
@@ -118,6 +118,21 @@ $(document).ready(function(){
 
     $('input[name=tel]').mask("+7 (999) 999-9999");
 
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({ 
+            type: "POST",
+            url: "./mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
 
 
   });
