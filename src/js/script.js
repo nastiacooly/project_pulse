f0a219d66bf4bc3d6920360ecbf69e860735937e
Anastasia <a.maryina@yandex.ru>
2020-07-24 16:55:48 +0300

    added smooth scroll script

--- src/js/script.js
+++ src/js/script.js
@@ -133,6 +133,23 @@ $(document).ready(function(){
         return false;
     });
 
+    // "To the top" button and Smooth scroll
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageUp').fadeIn();
+        } else {
+            $('.pageUp').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+
 
 
   });
